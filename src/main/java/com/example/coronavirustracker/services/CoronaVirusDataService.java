87a6ddbd117e3303e383ab596f449396804453dd
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-09-28 18:18:42 -0500

    feat: http response added with git repo coronavirus data and data now displaying in terminal

---
+++ src/main/java/com/example/coronavirustracker/services/CoronaVirusDataService.java
@@ -0,0 +1,51 @@
+package com.example.coronavirustracker.services;
+
+import com.example.coronavirustracker.models.LocationStats;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.xml.stream.Location;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CoronaVirusDataService {
+
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+    private List<LocationStats> allStats = new ArrayList<>();
+    @PostConstruct // telling Spring to execute method when application starts
+    @Scheduled(cron = "* * 1 * * *") // scheduling when to run the app
+    public void fetchVirusData() throws IOException, InterruptedException {
+        List<LocationStats> newStats = new ArrayList<>();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(VIRUS_DATA_URL))
+                .build();
+
+
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            LocationStats locationsStat = new LocationStats();
+            locationsStat.setState(record.get("Province/State"));
+            locationsStat.setCountry(record.get("Country/Region"));
+            locationsStat.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
+            System.out.println(locationsStat);
+            newStats.add(locationsStat);
+        }
+        this.allStats = newStats;
+    }
+}
